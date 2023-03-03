ed0d7d7ba412b399b480256c64c3801b67a7773e
RyanPeyrot <ryan.peyrot@gmail.com>
2023-03-03 19:38:00 +0100

    Commit

--- src/routes/place.route.js
+++ src/routes/place.route.js
@@ -5,7 +5,7 @@ const verifyToken = require('../middlewares/verifyToken');
 const verifyOwner = require('../middlewares/verifyOwner');
 
 router.get('/', placeController.getPlaces);
-router.get('/:id', placeController.getOnePlace);
+router.get('/one/:id', placeController.getOnePlace);
 router.post('/',verifyToken, verifyOwner, placeController.createOne);
 router.get('/myplaces', verifyToken, placeController.getMyPlaces);
 router.get('/myplace/:id',verifyToken, placeController.getMyPlace);
